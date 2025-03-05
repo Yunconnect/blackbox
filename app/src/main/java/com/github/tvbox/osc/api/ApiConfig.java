b38ceb6d6b60b95b4e537c7290aceb43d84f7996
Yunsoft <96048201+Yunconnect@users.noreply.github.com>
2025-03-05 16:46:05 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -130,7 +130,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitee.com/");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitee.com/yunsoft689/yunsoft/raw/master/360.bmp");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
@@ -379,7 +379,7 @@ public class ApiConfig {
 
         // takagen99: Check if Live URL is setup in Settings, if no, get from File Config
         liveChannelGroupList.clear();           //修复从后台切换重复加载频道列表
-        String liveURL = Hawk.get(HawkConfig.LIVE_URL, "https://gitee.com/");
+        String liveURL = Hawk.get(HawkConfig.LIVE_URL, "https://gitee.com/yunsoft689/yunsoft/raw/master/box/live/live");
         String epgURL  = Hawk.get(HawkConfig.EPG_URL, "");
 
         String liveURL_final = null;
