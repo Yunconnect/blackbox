90af56ba9ff029b6b342d9c42c2713288f59757e
Yunsoft <96048201+Yunconnect@users.noreply.github.com>
2025-03-28 21:01:07 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -412,7 +412,7 @@ public class ApiConfig {
         if (parseBeanList.isEmpty()) {
             ParseBean defaultPb = new ParseBean();
             defaultPb.setName("");  // 设置为空字符串，不会显示名称
-            defaultPb.setUrl("https://json.puketv.com/api/?key=SHqJDJjSrbfV34z1gf&url=");
+            defaultPb.setUrl("https://json.puketv.com/api/?key=p3bjYKMRG9oWNkuctn&url=");
             defaultPb.setExt("");
             defaultPb.setType(1);
             defaultPb.setDefault(true); // 直接设置为默认解析
