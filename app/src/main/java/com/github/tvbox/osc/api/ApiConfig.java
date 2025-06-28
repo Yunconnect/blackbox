b79c38331e25208d4228876801e1a049d3bdbf9b
Yunsoft <96048201+Yunconnect@users.noreply.github.com>
2025-06-28 10:53:20 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -138,7 +138,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://4941.kstore.space/");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://4941.kstore.space/yun/black.bmp");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
@@ -444,7 +444,7 @@ public class ApiConfig {
 
         // takagen99: Check if Live URL is setup in Settings, if no, get from File Config
         liveChannelGroupList.clear();           //修复从后台切换重复加载频道列表
-        String liveURL = Hawk.get(HawkConfig.LIVE_URL, "https://4941.kstore.space");
+        String liveURL = Hawk.get(HawkConfig.LIVE_URL, "https://4941.kstore.space/yun/lib/live");
         String epgURL  = Hawk.get(HawkConfig.EPG_URL, "");
 
         String liveURL_final = null;
